7fad32ee677a83ae36dd05d0fc748e4bd6b0aae6
Nicolas Serres <miracle@macbook-pro-de-nicolas.home>
2022-11-12 22:52:28 +0100

    push project

---
+++ src/pages/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, modi
+        tempora debitis culpa deserunt atque ratione a officia aliquam
+        laboriosam rem consequatur error amet nostrum veniam quasi. Earum
+        voluptatum quo at aut suscipit, ducimus architecto cumque libero
+        asperiores. Eum atque architecto maiores quae odit animi at tenetur
+        perspiciatis nulla voluptates sed, nemo, quas dolore, error beatae
+        molestiae fuga harum illum. Consectetur maiores sed repellat, inventore
+        impedit, iste ratione pariatur quaerat eligendi incidunt excepturi, aut
+        laboriosam at maxime velit! Doloremque neque exercitationem
+        reprehenderit consequuntur commodi similique eum repellat beatae
+        doloribus dolor reiciendis molestiae maxime cumque, ullam, architecto
+        aspernatur odio esse nostrum quasi dignissimos error? Magnam nesciunt
+        fuga maxime neque dolore odit at quasi, placeat voluptates illum nostrum
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+        aspernatur repudiandae ipsam dicta quae eligendi tenetur atque quis
+        harum accusantium fuga laboriosam delectus perspiciatis libero sed
+        recusandae modi, ipsum autem vero repellat! Quos, exercitationem eum vel
+        tenetur facilis dignissimos, beatae, earum hic fuga quia suscipit illum!
+        Quibusdam laborum eveniet natus quos eius aliquid, doloribus repellendus
+        veritatis minus mollitia doloremque reiciendis nam non quae autem nemo
+        similique repudiandae alias excepturi dignissimos ducimus est
+        blanditiis? Eum odio earum laudantium, nesciunt deleniti a dolores totam
+        recusandae quo. Sed quibusdam assumenda eius ipsam eaque at, sapiente
+        perspiciatis porro quis, illo pariatur ducimus illum omnis vel culpa
+        beatae minima facere ex in esse error eos hic tempora unde! Quod minima
+      </p>
+    </div>
+  );
+};
+
+export default About;
